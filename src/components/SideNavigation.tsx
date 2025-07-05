35d1a327fa719c14c730f7cf400a3a88b6cd7957
guotai <guotaixiao87@gmail.com>
2025-07-05 10:04:01 +1000

    feat: logout completed

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 export default function SideNavigation() {
+  const navigate = useNavigate();
+
   const token = localStorage.getItem("token");
   let userName = "Guest";
   if (token) {
     const decoded: any = jwtDecode(token);
     userName = decoded.userName;
+  }
+  function logoutHandler() {
+    localStorage.removeItem("token");
+    navigate("/");
   }
   return (
     <aside className="h-screen w-64 bg-green-300 text-black flex flex-col justify-between p-4 shadow-lg">
@@ -29,7 +35,9 @@ export default function SideNavigation() {
       </div>
 
       <div className="text-center">
-        <button className="hover:underline">Logout</button>
+        <button className="hover:underline" onClick={logoutHandler}>
+          Logout
+        </button>
       </div>
     </aside>
   );
