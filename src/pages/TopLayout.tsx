58fd78e0ed320f86892e221b0eb646e0592832b4
guotai <guotaixiao87@gmail.com>
2025-07-05 10:12:57 +1000

    feat: automatically restore login if valid token exists

--- src/pages/TopLayout.tsx
+++ src/pages/TopLayout.tsx
@@ -1,19 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 import AuthModal from "../components/Auth";
 import TopNavigation from "../components/TopNavigation";
 
 export default function TopLayout() {
+  const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
   const [isLogin, setIslogin] = useState(true);
 
   function handleShowLogin() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded: any = jwtDecode(token);
+      navigate(`/${decoded.userId}`);
+    }
     setIslogin(true);
     setShowModal(true);
   }
 
   function handleShowSignup() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      localStorage.removeItem("token");
+    }
     setIslogin(false);
     setShowModal(true);
   }
