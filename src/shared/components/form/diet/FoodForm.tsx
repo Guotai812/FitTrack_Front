f6f6f7a3c330f1dc4a3b1c2b38c4c43505c990ec
guotai <guotaixiao87@gmail.com>
2025-07-22 09:07:28 +1000

    feat: send http to add user's diet to backend

--- src/shared/components/form/diet/FoodForm.tsx
+++ src/shared/components/form/diet/FoodForm.tsx
@@ -1,15 +1,24 @@
+const baseUrl = import.meta.env.VITE_BACKEND_URL;
+import type React from "react";
 import { useDiet } from "../../../context/DietManageContext";
 import { useFood } from "../../../context/FoodContext";
 import { usePool } from "../../../context/PoolConetext";
 import { useForm } from "../../../hooks/useForm/useForm";
+import { useAuth } from "../../../context/AuthContext";
+import useHttp from "../../../hooks/useHttp";
 import useInput from "../../../hooks/useInput";
 import validator from "../../../util/validator";
 
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import ErrorModal from "../../ui/ErrorModal";
+import { useModal } from "../../../hooks/useModal";
 
 export default function FoodList() {
+  const { show, modalCancelHandler, modalDisplayHandler } = useModal();
+  const { user, token } = useAuth();
+  const { error, isLoading, sendRequest } = useHttp();
   const { formState, inputHandler } = useForm(
     {
       weight: {
@@ -27,8 +36,28 @@ export default function FoodList() {
   const clickedFood = Object.values(pool).find((food) => food._id === foodId);
   const weight = Number(formState.inputs.weight.value) / 100;
 
+  async function submitHandler(e: React.FocusEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      await sendRequest({
+        url: `${baseUrl}/basic/${user?.userId}/addDiet`,
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+        body: { id: clickedFood?._id, weight: formState.inputs.weight.value },
+      });
+    } catch (err) {
+      modalDisplayHandler();
+    }
+  }
+
+  if (error && show) {
+    return (
+      <ErrorModal title="Failed!" msg={error} onCancel={modalCancelHandler} />
+    );
+  }
+
   return (
-    <Form isStyled={false} className="w-4/5">
+    <Form isStyled={false} className="w-4/5" onSubmit={submitHandler}>
       <input type="hidden" name="name" defaultValue={clickedFood?.name} />
       <div className="h-full p-6 flex flex-col justify-end ">
         <div className="h-full">
@@ -72,7 +101,7 @@ export default function FoodList() {
                 )
               }
             />
-            <>g</>
+            <p>g</p>
           </div>
         </div>
 
@@ -80,7 +109,7 @@ export default function FoodList() {
           <Button kind="cancel" onClick={() => setState("pool")}>
             Cancel
           </Button>
-          <Button kind="confirm" disabled={!formState.isValid}>
+          <Button kind="confirm" disabled={!formState.isValid || isLoading}>
             Confirm
           </Button>
         </div>
